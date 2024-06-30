088387cd686f03d72845dae181a6d42679a52296
IldarZay <zayildar@gmail.com>
2024-06-30 18:50:44 +0400

    попытка добавить Allure

--- src/test/java/TestSauceDemo.java
+++ src/test/java/TestSauceDemo.java
@@ -1,22 +1,29 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 
+@Epic("SauceDemo тесты")
+@Feature("Вход")
 public class TestSauceDemo {
     WebDriver driver;
     StartPageSauceDemo startPage;
 
     @BeforeClass
+    @Step("Инициализация драйвера")
     public void setUp() {
         driver = new ChromeDriver();
         startPage = new StartPageSauceDemo(driver);
     }
 
     @Test
+    @Description("Негативная проверка логин")
     public void nocorrectLog() {
         startPage.openSite();
         startPage.logIn("Test", "secret_sauce");
@@ -24,6 +31,7 @@ public class TestSauceDemo {
     }
 
     @Test
+    @Description("Негативная проверка пароль")
     public void nocorrectPass() {
         startPage.openSite();
         startPage.logIn("standard_user", "Test");
@@ -31,12 +39,15 @@ public class TestSauceDemo {
     }
 
     @Test
+    @Description("Проверка если в самом тесте неверно указан текст")
     public void nocorrectPassbadExpected() {
         startPage.openSite();
         startPage.logIn("standard_user", "Test");
         assertEquals(startPage.getErrorMessage(), "Test text", "Ошибка");
     }
+
     @Test
+    @Description("Проверка на пустой логин пароль")
     public void cleanLogandPass() {
         startPage.openSite();
         startPage.logIn("", "");
@@ -44,6 +55,7 @@ public class TestSauceDemo {
     }
 
     @AfterClass
+    @Step("Закрытие")
     public void tearDown() {
         driver.quit();
     }
