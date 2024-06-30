86adc5bdf90261cb4481f23bc6389e9caa5994f7
IldarZay <zayildar@gmail.com>
2024-06-30 18:03:14 +0400

    add StartPage method getErrorMessage

--- src/test/java/StartPageSauceDemo.java
+++ src/test/java/StartPageSauceDemo.java
@@ -2,23 +2,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class StartPageSauceDemo {
-    WebDriver runMethodWebdriver;
-    private final By Username = By.id("user-name");
-    private final By Password = By.id("password");
-    private final By Button_Click = By.id("login-button");
+    WebDriver driver;
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login-button");
 
-    public StartPageSauceDemo(WebDriver runMethodWebdriver) {
-        this.runMethodWebdriver = runMethodWebdriver;
+    public StartPageSauceDemo(WebDriver driver) {
+        this.driver = driver;
     }
 
     public void openSite() {
-        runMethodWebdriver.get("https://www.saucedemo.com/");
+        driver.get("https://www.saucedemo.com/");
     }
 
-    // Method to log in
-    public void logIn(String user, String password) {
-        runMethodWebdriver.findElement(Username).sendKeys(user);
-        runMethodWebdriver.findElement(Password).sendKeys(password);
-        runMethodWebdriver.findElement(Button_Click).click();
+    public void logIn(String username, String password) {
+        driver.findElement(usernameField).sendKeys(username);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(By.cssSelector("[data-test='error']")).getText();
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
     }
 }
